f4de89e1136ef82c145f47dc3a9a5d22ee280a82
Lynne Daniels <lynne33477@gmail.com>
2019-01-11 15:57:12 -0500

    feat(spotAPI) downloads 50 most recent messages from findmespot

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const request = require('request');
 const port = 3000;
 
+const spotURL = 'https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/0V0CEA8FAAMvZ4ruaZ5PHxbAZO197Sr0e/message.json'
+
 app.get('/', (req, res) => res.send('Hello World'))
+
+app.get('/spot', (req, res) => {
+  request(spotURL, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body);
+    console.log('as JSON',JSON.parse(body));
+    res.send(body);
+  })
+  //.then(res.send(body)); --> body not defined
+  // res.send('TODO Data goes here -- test nodemon');
+})
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
