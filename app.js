87019c6113b6af998f1bb4f7cbe4a5ebf83de136
Lynne Daniels <lynne33477@gmail.com>
2019-01-13 23:07:10 -0500

    feat(updates): only uses spot API if DB data is more than 3 min old.

--- app.js
+++ app.js
@@ -11,21 +11,43 @@ app.get('/', (req, res) => res.send('Hello World'))
 
 app.get('/spot', (req, res) => {
   // TODO if mongo has data, do not request from spotURL
+  const now = Date.now();
+  const updateCurrentTrackPoints = (isCurrent) => {
+    // isCurrent - use db
+    if (isCurrent) {
+      console.log('will do isCurrent stuff')
+      //TODO have isCurrent return unixTime of most recent track in DB
+      // TODO sort out pagination, multiple requests for big sets. yuk.
+      // Do people do that by hand or should I get a library?
+      db.getTracks(1, Date.now(), (tracks) => {
+        res.send(tracks);
+      })
+    } else {
+      console.log('will do *not* isCurrent stuff')
+      // !isCurrent - fetch from API
+      request(spotURL, (error, response, body) => {
+        if (error) {
+          console.log('error:', error);
+        }
+        const tracks = JSON.parse(body).response.feedMessageResponse.messages.message;
+        const uniqueTracks = tracks.map((track)=> {
+          let output = {...track};
+          output._id = track.id;
+          return output;
+        });
+    
+        iterateFunc = (doc) => {
+          console.log('record: ', JSON.stringify(doc, null, 4));
+        }
+        db.getTracks(1547312738, 1547321133, iterateFunc)
+        res.send(body);
+      })
+    }
+  }
+  db.hasCurrent(now -180, now, updateCurrentTrackPoints)
   // have to rate limit spot requests to > 2.5 min between calls for sameid
   // remember if using diff ids, need 2 sec rate limit btwn calls
-  request(spotURL, (error, response, body) => {
-    if (error) {
-      console.log('error:', error);
-    }
-    const tracks = JSON.parse(body).response.feedMessageResponse.messages.message;
-    const uniqueTracks = tracks.map((track)=> {
-      let output = {...track};
-      output._id = track.id;
-      return output;
-    });
-    db.addTracks(uniqueTracks); // saves iff _id is not a duplicate
-    res.send(body);
-  })
+  // res.send('comment this out later');
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
--- db.js
+++ db.js
@@ -17,10 +17,45 @@ const addTracks = (newTracks) => {
     client.close();
   });
 }
-// TODO retreive tracks
 
+
+const getTracks = (start, end, cb) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    const db = client.db('tracks');
+    const records = db.collection('points').find({
+      $and: [{unixTime: {$lt: end + 1}},{unixTime: {$gt: start - 1}}]
+    })
+    console.log('records.length: ', records.length)
+
+   
+   function errorFunc(error) {
+      console.log('error: ', error);
+   }
+   
+   records.forEach(cb, errorFunc);
+    client.close();
+  })
+}
+
+const hasCurrent = (start, end, cb) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    const db = client.db('tracks');
+    console.log('in here now')
+    const numRecs = db.collection('points').countDocuments({
+      $and: [{unixTime: {$lt: end + 1}},{unixTime: {$gt: start - 1}}]
+    })
+    .then((count) => {
+      console.log('COUNT! ', count);
+      cb(count > 0);
+    })
+    // console.log(numRecs);
+    // const isCurrent = numRecs > 0;
+  })
+}
 const db = {
-  addTracks
+  addTracks,
+  getTracks,
+  hasCurrent
 }
 
 module.exports = db;
